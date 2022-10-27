0804350be934f5e0c0687e1da762fc40194da80c
Kishore Ch <kishu.559@gmail.com>
2022-10-27 23:48:37 +0530

    pattern5and6

--- pattern.js
+++ pattern.js
@@ -32,3 +32,21 @@ for (i = 1; i <= 5; i++) {
   }
   console.log(str);
 }
+
+console.log("Pattern.5");
+for (i = 5; i >= 1; i--) {
+  var str = "";
+  for (j = 1; j <= i; j++) {
+    str = str + "*";
+  }
+  console.log(str);
+}
+console.log("Pattern.6");
+
+for (i = 5; i >= 1; i--) {
+  var str = "";
+  for (j = 1; j <= i; j++) {
+    str = str + j;
+  }
+  console.log(str);
+}
