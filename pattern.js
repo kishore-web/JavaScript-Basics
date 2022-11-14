94a7f366146635f23a9ea669d713ef23e4716e63
Kishore Cherukuru <114729247+kishore-web@users.noreply.github.com>
2022-11-14 23:01:45 +0530

    Update pattern.js

--- pattern.js
+++ pattern.js
@@ -201,3 +201,18 @@ for(i = 5;i>=1;i--){
   }
   console.log(str)
 }
+// console.log("Pattern.20")
+for(i = 1;i<=5;i++){
+  var str = "";
+  for(j=1; j<= i; j++){
+    str = str +"*"
+  }
+  console.log(str)
+}
+for(i = 4;i>=1;i--){
+  var str = "";
+  for(j=1; j<= i; j++){
+    str = str +"*"
+  }
+  console.log(str)
+}
